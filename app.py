50d574430d5c7e1f8dc1e847990f4c4daf2127d0
Juan Andres Porras <juanandresporras@gmail.com>
2025-11-04 12:02:31 -0500

    Try 6 - Added Session Token

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hola Mundo 2 desde Flask en EC2! <br> ¡Despliegue exitoso! - Intento 5"
+    return "Hola Mundo 2 desde Flask en EC2! <br> ¡Despliegue exitoso! - Intento 6"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
